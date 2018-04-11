2f7827a1847feaea8aac4caff6e90d94a2f56fb3
Eduardo Macêdo <eduardo@popcode.com.br>
2018-04-11 13:32:14 -0300

    standard added and peerDependencies updated

--- RealmPersistInterface.js
+++ RealmPersistInterface.js
@@ -1,93 +1,97 @@
-import Realm from 'realm';
+import Realm from 'realm'
 
 class RealmPersistInterface {
-    constructor() {
-        this.realm = new Realm({
-            schema: [{
-                name: 'Item',
-                primaryKey: 'name',
-                properties: {
-                    name: 'string',
-                    content: 'string',
-                },
-            }],
-        });
+  constructor () {
+    this.realm = new Realm({
+      schema: [{
+        name: 'Item',
+        primaryKey: 'name',
+        properties: {
+          name: 'string',
+          content: 'string'
+        }
+      }]
+    })
 
-        this.items = this.realm.objects('Item');
-    }
+    this.items = this.realm.objects('Item')
+  }
 
-    getItem = key => new Promise((resolve, reject) => {
-        try {
-            const matches = this.items.filtered(`name = "${key}"`);
+  getItem (key) {
+    return new Promise((resolve, reject) => {
+      const matches = this.items.filtered(`name = "${key}"`)
 
-            if (matches.length > 0 && matches[0]) {
-                resolve(matches[0].content);
+      if (matches.length > 0 && matches[0]) {
+        resolve(matches[0].content)
+      } else {
+        reject(new Error(`Could not get item with key: '${key}'`))
+      }
+    })
+  }
+
+  setItem (key, value) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.getItem(key, (error) => {
+          this.realm.write(() => {
+            if (error) {
+              this.realm.create(
+                'Item',
+                {
+                  name: key,
+                  content: value
+                }
+              )
             } else {
-                throw new Error(`Could not get item with key: '${key}'`);
+              this.realm.create(
+                'Item',
+                {
+                  name: key,
+                  content: value
+                },
+                true
+              )
             }
-        } catch (error) {
-            reject(error);
-        }
-    });
 
-    setItem = (key, value) => new Promise((resolve, reject) => {
-        try {
-            this.getItem(key, (error) => {
-                this.realm.write(() => {
-                    if (error) {
-                        this.realm.create(
-                            'Item',
-                            {
-                                name: key,
-                                content: value,
-                            }
-                        );
-                    } else {
-                        this.realm.create(
-                            'Item',
-                            {
-                                name: key,
-                                content: value,
-                            },
-                            true
-                        );
-                    }
+            resolve()
+          })
+        })
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
 
-                    resolve();
-                });
-            });
-        } catch (error) {
-            reject(error);
-        }
-    });
+  removeItem (key) {
+    return new Promise((resolve, reject) => {
+      try {
+        this.realm.write(() => {
+          const item = this.items.filtered(`name = "${key}"`)
 
-    removeItem = key => new Promise((resolve, reject) => {
-        try {
-            this.realm.write(() => {
-                const item = this.items.filtered(`name = "${key}"`);
+          this.realm.delete(item)
 
-                this.realm.delete(item);
+          resolve()
+        })
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
 
-                resolve();
-            });
-        } catch (error) {
-            reject(error);
-        }
-    });
+  getAllKeys () {
+    return new Promise((resolve, reject) => {
+      try {
+        const keys = this.items.map(
+          (item) => item.name
+        )
 
-    getAllKeys = () => new Promise((resolve, reject) => {
-        try {
-            const keys = this.items.map(
-                (item) => item.name
-            );
-
-            resolve(keys);
-        } catch (error) {
-            reject(error);
-        }
-    });
+        resolve(keys)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
 }
 
-const singleton = new RealmPersistInterface();
+const singleton = new RealmPersistInterface()
 
-export default singleton;
+export default singleton
